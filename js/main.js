68491024c027cd249226ce99b6a8d2b2196507b5
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-01-07 15:08:51 +0100

    header and nav finished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const allNavLink = document.querySelectorAll('.nav__link')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+
+	allNavLink.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
+
+// funkcja aktualizaująca rok w footerze
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
